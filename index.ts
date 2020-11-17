277f6b8bd82eabb803c1c0c917409fe139dc7d44
edupes <yo.ey.abc@gmail.com>
2020-11-17 16:55:01 +0100

    fix homedir resolution

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import * as path from "path";
 import * as fs from "fs";
+import * as os from "os";
 
 export function main() {
 	let parameters = process.argv.slice(1);
-	const root = path.resolve("~", ".vlcluster");
+	const root = path.resolve(os.homedir(), ".vlcluster");
 
 	console.log(`[vlc] root ${root}`);
 
