7886d72998776ffa784cb4ecf7e7d8cd6e159e41
edupes <yo.ey.abc@gmail.com>
2020-11-17 16:43:13 +0100

    add main wrapper

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import * as path from "path";
 import * as fs from "fs";
 
-let parameters = process.argv.slice(1);
-const root = path.resolve("~/.vlcluster");
+export function main() {
+	let parameters = process.argv.slice(1);
+	const root = path.resolve("~/.vlcluster");
 
-if (!fs.existsSync(root)) {
-	fs.mkdirSync(root);
+	console.log(`[vlc] root ${root}`);
+
+	if (!fs.existsSync(root)) {
+		fs.mkdirSync(root);
+	}
+
+	console.log(parameters);
 }
-
-console.log(parameters);
