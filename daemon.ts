8e38101dbe7b3ed7656222925e46b9067354f804
Levi Hechenberger Di Valentino <68319554+levvij@users.noreply.github.com>
2021-01-20 11:55:59 +0100

    fix dim logo version

--- daemon.ts
+++ daemon.ts
@@ -13,7 +13,7 @@ export class Daemon {
 	async start() {
 		const logger = new Logger("daemon");
 
-		process.stdout.write(`\u001b[1m${Cluster.logo}\u001b[0m`);
+		process.stdout.write(`\u001b[1m${Cluster.logo}  \u001b[2m${Cluster.version}\u001b[0m`);
 
 		logger.log("starting vlcluster daemon server");
 
