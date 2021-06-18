a28968ac35e7754b0fa9173c8960e20a7d6ab674
edupes <yo.ey.abc@gmail.com>
2021-06-18 11:02:59 +0200

    add gateway update logging

--- gateway/gateway.ts
+++ gateway/gateway.ts
@@ -125,7 +125,9 @@ export class GatewayServer {
 				this.logger.hp(route.host, route.port), 
 				" → ", 
 				this.logger.ae(route.application, route.env), 
-				` (${route.ssl ? "SSL, " : ""}${route.instances.length} instances${route.sockets.length ? `, ${route.sockets.length} websockets` : ""})`
+				` (${route.ssl ? "SSL, " : ""}${route.instances.length} instances [`,
+				...route.instances.map(i => this.logger.hp(i.endpoint, i.port)),
+				``]${route.sockets.length ? `, ${route.sockets.length} websockets` : ""})`
 			);
 
             // create upstream
