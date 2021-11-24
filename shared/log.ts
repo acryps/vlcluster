942c34a0231b002996966760eccf89cbd646f35c
Levi Hechenberger Di Valentino <levi.hechenberger@gmail.com>
2021-11-24 09:18:07 +0100

    fix stepping method

--- shared/log.ts
+++ shared/log.ts
@@ -106,7 +106,7 @@ export class Logger {
                         bar += percentage[i - 1];
                     } else {
                         if (i == field - 1) {
-                            bar += Math.floor((current % (length / total)) * bars.length);
+                            bar += Math.floor((current % (total / length) / (total / length)) * bars.length);
                         } else {
                             bar += " ";
                         }
